45202d3ee9bf49a4ca58cbbafe9d96bba8435e06
Gy0m <landreaug@gmail.com>
2016-11-22 17:15:10 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Evaluation
+==========
+
+A Symfony project created on November 22, 2016, 2:08 pm.
+# Media
---
+++ src/MediaBundle/Entity/Album.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace MediaBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Album
+ *
+ * @ORM\Table(name="album")
+ * @ORM\Entity(repositoryClass="MediaBundle\Repository\AlbumRepository")
+ */
+class Album
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre_album", type="string", length=255)
+     */
+    private $titreAlbum;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="artiste", type="string", length=255)
+     */
+    private $artiste;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="genre", type="string", length=255)
+     */
+    private $genre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="support", type="string", length=255)
+     */
+    private $support;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titreAlbum
+     *
+     * @param string $titreAlbum
+     * @return Album
+     */
+    public function setTitreAlbum($titreAlbum)
+    {
+        $this->titreAlbum = $titreAlbum;
+
+        return $this;
+    }
+
+    /**
+     * Get titreAlbum
+     *
+     * @return string 
+     */
+    public function getTitreAlbum()
+    {
+        return $this->titreAlbum;
+    }
+
+    /**
+     * Set artiste
+     *
+     * @param string $artiste
+     * @return Album
+     */
+    public function setArtiste($artiste)
+    {
+        $this->artiste = $artiste;
+
+        return $this;
+    }
+
+    /**
+     * Get artiste
+     *
+     * @return string 
+     */
+    public function getArtiste()
+    {
+        return $this->artiste;
+    }
+
+    /**
+     * Set genre
+     *
+     * @param string $genre
+     * @return Album
+     */
+    public function setGenre($genre)
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    /**
+     * Get genre
+     *
+     * @return string 
+     */
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+
+    /**
+     * Set support
+     *
+     * @param string $support
+     * @return Album
+     */
+    public function setSupport($support)
+    {
+        $this->support = $support;
+
+        return $this;
+    }
+
+    /**
+     * Get support
+     *
+     * @return string 
+     */
+    public function getSupport()
+    {
+        return $this->support;
+    }
+
+	/**
+	 * @ORM\OneToMany(targetEntity="Commentaire", mappedBy="album", cascade={"remove", "persist"})
+	 */
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->commentaire = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add commentaire
+     *
+     * @param \MediaBundle\Entity\Commentaire $commentaire
+     * @return Album
+     */
+    public function addCommentaire(\MediaBundle\Entity\Commentaire $commentaire)
+    {
+        $this->commentaire[] = $commentaire;
+
+        return $this;
+    }
+
+    /**
+     * Remove commentaire
+     *
+     * @param \MediaBundle\Entity\Commentaire $commentaire
+     */
+    public function removeCommentaire(\MediaBundle\Entity\Commentaire $commentaire)
+    {
+        $this->commentaire->removeElement($commentaire);
+    }
+
+    /**
+     * Get commentaire
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getCommentaire()
+    {
+        return $this->commentaire;
+    }
+
+
+	/**
+	 * Set commentaire
+	 *
+	 * @param \Doctrine\Common\Collections\Collection $commentaire
+	 */
+	public function setCommentaire(\Doctrine\Common\Collections\Collection $commentaire)
+	{
+		$this->commentaire = $commentaire;
+	}
+
+	/**
+	 * @param int $id
+	 */
+	public function setId($id)
+	{
+		$this->id = $id;
+	}
+
+}
